2304f7af2f646846b34b786e72ab458192f5f582
progress0407 <progress0407@gmail.com>
2023-08-07 21:24:33 +0900

    Simple Kafka Producer 리펙터링

--- kafka/src/main/kotlin/philo/simple/SimpleProducer.kt
+++ kafka/src/main/kotlin/philo/simple/SimpleProducer.kt
@@ -11,35 +11,50 @@ import java.util.*
 
 private val log: KLogger = KotlinLogging.logger {}
 
-class SimpleProducer {
+class SimpleProducer<K, V> {
 
     private val log: KLogger = KotlinLogging.logger {}
 
-    init {
-        log.infoGreen { "Hello World" }
+    private val kafkaProducer: KafkaProducer<K, V> = initKafkaProducer()
+
+    private fun initKafkaProducer(): KafkaProducer<K, V> {
+        val configs = initConfigs()
+        log.infoGreen { "Kafka Construct !!" }
+        return KafkaProducer<K, V>(configs)
+    }
+
+    private fun initConfigs(): Properties {
+        val configs = Properties()
+        configs[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = BOOTSTRAP_SERVERS
+        configs[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
+        configs[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
+        return configs
     }
 
     companion object {
         public const val TOPIC_NAME = "test-topic"
         public const val BOOTSTRAP_SERVERS = "localhost:9092"
     }
+
+    fun sendValue(value: V) {
+        val record = createRecord(value)
+        kafkaProducer.send(record)
+        log.infoGreen { "send record :  $record" }
+
+    }
+
+    fun closeAndFlush() {
+        kafkaProducer.flush()
+        kafkaProducer.close()
+    }
+
+    private fun createRecord(value: V) = ProducerRecord<K, V>(TOPIC_NAME, value)
 }
 
 fun main() {
-    val simpleProducer = SimpleProducer()
+    val producer = SimpleProducer<String, String>()
 
-    val configs = Properties()
-    configs[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = SimpleProducer.BOOTSTRAP_SERVERS
-    configs[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
-    configs[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
+    producer.sendValue("testMessage")
 
-    val kafkaProducer = KafkaProducer<String, String>(configs)
-
-    val messageValue = "testMessage"
-
-    val record = ProducerRecord<String, String>(SimpleProducer.TOPIC_NAME, messageValue)
-    kafkaProducer.send(record)
-    log.infoGreen { "send record :  ${record}" }
-    kafkaProducer.flush()
-    kafkaProducer.close()
+    producer.closeAndFlush()
 }
