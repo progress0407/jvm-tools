e39c891e9c13689f7ea94b14e1d13337e5d170fc
progress0407 <progress0407@gmail.com>
2023-11-03 23:46:55 +0900

    add msa modules (eureka, gateway...) (in progress)

---
+++ msa/eureka/src/main/kotlin/com/philo/EurekaApp.kt
@@ -0,0 +1,13 @@
+package com.philo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaApp
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EurekaApp::class.java, *args)
+}
