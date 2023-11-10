4b6e83c2ec3a0aead3643b767ab97ad0bc02ed6f
progress0407 <progress0407@gmail.com>
2023-11-10 14:03:12 +0900

    git add temp files

--- msa/ms-1/src/main/kotlin/com/philo/JustController.kt
+++ msa/ms-1/src/main/kotlin/com/philo/JustController.kt
@@ -1,11 +1,13 @@
 package com.philo
 
 import mu.KotlinLogging
+import org.springframework.cloud.client.ServiceInstance
+import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class JustController {
+class JustController(val discoveryClient: DiscoveryClient) {
 
     private val log = KotlinLogging.logger { }
 
@@ -19,5 +21,26 @@ class JustController {
     fun just(): String {
         log.info { "" }
         return "ok"
+    }
+
+    @GetMapping("/test/instances")
+    fun instances(): MutableList<ServiceInstance>? {
+        return discoveryClient.getInstances("micro-service-1");
+    }
+
+    @GetMapping("/test/instances-2")
+    fun instances2(): Map<String, List<String>> {
+        return discoveryClient.services.associateWith { serviceId ->
+            discoveryClient.getInstances(serviceId).map {
+                it.uri.toString()
+            }
+        }
+    }
+
+    @GetMapping("/test/instances-3")
+    fun instances3(): Map<String, MutableList<ServiceInstance>> {
+        return discoveryClient.services.associateWith { serviceId ->
+            discoveryClient.getInstances(serviceId)
+        }
     }
 }
