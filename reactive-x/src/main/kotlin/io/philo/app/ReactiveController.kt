1be4946b00c6954afc643b823e6dddcc7f3ecf4c
progress0407 <progress0407@gmail.com>
2023-12-28 22:39:16 +0900

    Reactive-X 모듈 생성

---
+++ reactive-x/src/main/kotlin/io/philo/app/ReactiveController.kt
@@ -0,0 +1,24 @@
+package io.philo.app
+
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class ReactiveController {
+
+    @GetMapping("/reactive-x")
+    fun _1(servletResponse: ServletRequest) {
+
+        val httpServletResponse = servletResponse as HttpServletResponse
+        httpServletResponse.contentType = "text/plain;charset=utf-8"
+        val out = httpServletResponse.writer
+
+        out.println("응답")
+
+        out.flush()
+    }
+}
