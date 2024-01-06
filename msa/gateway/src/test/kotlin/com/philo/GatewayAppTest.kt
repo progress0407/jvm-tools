bc4e0bdcb7dbfc23f0779e0419f6953b4d4d1c04
progress0407 <progress0407@gmail.com>
2024-01-06 19:01:16 +0900

    [ Spring Cloud Gateway ] 검증 로직 리펙터링 (by infix, Test Fixture)

--- msa/gateway/src/test/kotlin/com/philo/GatewayAppTest.kt
+++ msa/gateway/src/test/kotlin/com/philo/GatewayAppTest.kt
@@ -12,10 +12,10 @@ import org.springframework.cloud.gateway.filter.OrderedGatewayFilter
 import org.springframework.cloud.gateway.route.Route
 import org.springframework.cloud.gateway.route.RouteLocator
 import kotlin.reflect.KClass
+import com.philo.GatewayAppTest.TestRoute as Route_
 
 @SpringBootTest
 class GatewayAppTest {
-
     @Autowired
     lateinit var routeLocator: RouteLocator
 
@@ -27,27 +27,38 @@ class GatewayAppTest {
 
         val routes: MutableList<Route> = routeLocator.routes.collectList().block()!!
 
-        val itemRoute = routes.find { it.msId == "ITEM-SERVICE" }!!
-        itemRoute.predicate.toString() shouldContain "/items"
-        itemRoute.filters shouldHasType AuthFilter::class
+        routes["ITEM-SERVICE"] shouldBe_ Route_("/item", AuthFilter::class)
 
 //        val orderRoute = routes.find { it.msId == "ORDER-SERVICE" }!!
 //        orderRoute.predicate.toString() shouldContain "/orders"
 //        orderRoute.filters shouldContain loggingFilter
     }
 
-    final inline infix fun <T : List<GatewayFilter>, reified U: GatewayFilter>
-            T.shouldHasType(expected: KClass<U>) {
+    final inline infix fun <reified F: GatewayFilter> Route.shouldBe_(route: Route_<F>) {
+        this.predicate.toString() shouldContain route.path
+        if(route.filter != null)
+            this.filters shouldHasFilter route.filter!!
+    }
+
+    final inline infix fun <T : List<GatewayFilter>, reified U : GatewayFilter>
+            T.shouldHasFilter(expected: KClass<U>) {
 
         val isMatched = this.map { gatewayFilter -> gatewayFilter as OrderedGatewayFilter }
             .any { orderedGatewayFilter -> orderedGatewayFilter.delegate is U }
 
         isMatched shouldBe true
     }
+
+    private val Route.msId: String
+        get() = this.uri.toString().replaceFirst("lb://", "")
+
+    class TestRoute<F: GatewayFilter>(var path: String, var filter: KClass<F>?) {
+
+    }
+
+    private operator fun <T: Route> MutableList<T>.get(msId: String): T {
+        return this.find { it.msId == msId }!!
+    }
 }
-
-
-private val Route.msId: String
-    get() = this.uri.toString().replaceFirst("lb://", "")
 
 
