9eea1ed37a005605c3b4f4dcf579cc7b41401a3d
progress0407 <progress0407@gmail.com>
2024-01-06 18:45:25 +0900

    [ Spring Cloud Gateway ] Filter 검증 로직 리펙터링

--- msa/gateway/src/test/kotlin/com/philo/GatewayAppTest.kt
+++ msa/gateway/src/test/kotlin/com/philo/GatewayAppTest.kt
@@ -7,9 +7,11 @@ import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.gateway.filter.GatewayFilter
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter
 import org.springframework.cloud.gateway.route.Route
 import org.springframework.cloud.gateway.route.RouteLocator
+import kotlin.reflect.KClass
 
 @SpringBootTest
 class GatewayAppTest {
@@ -27,14 +29,20 @@ class GatewayAppTest {
 
         val itemRoute = routes.find { it.msId == "ITEM-SERVICE" }!!
         itemRoute.predicate.toString() shouldContain "/items"
-        val hasAuthFilter = itemRoute.filters
-            .map { gatewayFilter -> gatewayFilter as OrderedGatewayFilter }
-            .any { orderedGatewayFilter -> orderedGatewayFilter.delegate is AuthFilter }
-        hasAuthFilter shouldBe true
+        itemRoute.filters shouldHasType AuthFilter::class
 
 //        val orderRoute = routes.find { it.msId == "ORDER-SERVICE" }!!
 //        orderRoute.predicate.toString() shouldContain "/orders"
 //        orderRoute.filters shouldContain loggingFilter
+    }
+
+    final inline infix fun <T : List<GatewayFilter>, reified U: GatewayFilter>
+            T.shouldHasType(expected: KClass<U>) {
+
+        val isMatched = this.map { gatewayFilter -> gatewayFilter as OrderedGatewayFilter }
+            .any { orderedGatewayFilter -> orderedGatewayFilter.delegate is U }
+
+        isMatched shouldBe true
     }
 }
 
