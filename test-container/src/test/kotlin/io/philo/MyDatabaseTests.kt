03e4308bb53d49d306f7d5ac66ec6872b44c1f72
progress0407 <progress0407@gmail.com>
2024-01-10 09:09:22 +0900

    `test-container` 뼈대 코드 작성

---
+++ test-container/src/test/kotlin/io/philo/MyDatabaseTests.kt
@@ -0,0 +1,26 @@
+package io.philo
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.string.shouldNotBeBlank
+import org.testcontainers.containers.PostgreSQLContainer
+
+
+//@Testcontainer
+class MyDatabaseTests : StringSpec({
+
+    val postgresContainer: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:latest")
+
+    "테스트 컨테이너 구동" {
+        postgresContainer.start()
+
+        val jdbcUrl = postgresContainer.getJdbcUrl()
+        val username = postgresContainer.username
+        val password = postgresContainer.password
+
+        jdbcUrl.shouldNotBeBlank()
+        username.shouldNotBeBlank()
+        password.shouldNotBeBlank()
+
+        postgresContainer.stop()
+    }
+})
