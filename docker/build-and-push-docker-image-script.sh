89a3973ec47f87f3b6d444accdc4760ab415c161
progress0407 <progress0407@gmail.com>
2024-01-13 23:31:46 +0900

    [Docker 모듈] docker script에서 환경 변수 분리

--- docker/build-and-push-docker-image-script.sh
+++ docker/build-and-push-docker-image-script.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+#
+# input example.
+# `sh build-and-push-docker-image-script.sh local-mem`
+#
+
 set -e
 source ./color-echo.sh
 
+SPRING_PROFILE=${1:-prod}  # Default to 'prod' if no argument provided
 DOCKER_IMAGE_NAME="progress0407/docker-app"
 
 echo_blue "[ Building Jar file ... ]"
 ../gradlew :docker:assemble
 
 echo_blue "[ Building Docker image: $DOCKER_IMAGE_NAME ... ]"
-docker build -t $DOCKER_IMAGE_NAME -f docker-script/app/Build-Jar-Dockerfile .
+docker build \
+    -t $DOCKER_IMAGE_NAME \
+    --build-arg SPRING_PROFILE="$SPRING_PROFILE" \
+    -f docker-script/app/Build-Jar-Dockerfile .
 
 echo_blue "[ Pushing Docker image: $DOCKER_IMAGE_NAME ... ]"
 docker push $DOCKER_IMAGE_NAME
