3b9fe41666750fc6fb1c0f132462688af021b1e3
progress0407 <progress0407@gmail.com>
2024-01-18 18:20:27 +0900

    [Docker Module] add current package as bean dir

--- docker/src/main/kotlin/io/philo/docker/DockerApp.kt
+++ docker/src/main/kotlin/io/philo/docker/DockerApp.kt
@@ -3,7 +3,7 @@ package io.philo.docker
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["io.philo.app"])
+@SpringBootApplication(scanBasePackages = ["io.philo.app", "io.philo.docker"])
 class DockerApp
 
 fun main() {
