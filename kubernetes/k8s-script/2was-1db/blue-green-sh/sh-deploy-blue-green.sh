32b4c94b1ec86364fe624b18cf771b5ba5406eb6
progress0407 <progress0407@gmail.com>
2024-01-21 00:35:54 +0900

    주석 추가

--- kubernetes/k8s-script/2was-1db/blue-green-sh/sh-deploy-blue-green.sh
+++ kubernetes/k8s-script/2was-1db/blue-green-sh/sh-deploy-blue-green.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# assign k keyword
 k="minikube kubectl -- "
 
-# kg -n pod_name pods -l instance='?', blue-green-type='blue' -o jsonpath='{.items[*].status.containerStatuses[*].ready}'
-
+# blue deploy script
 # ka deploy-was-blue.yml
 # ka svc-nodeport-blue.yml
 
+# deploy blue
 $k apply -f deploy-was-green.yml
 
+# wait while blue deploy
 while true; do
 
     # status=$(curl -s "$(minikube ip):30000/actuator/health")
@@ -22,12 +24,16 @@ while true; do
     sleep 1;
 done;
 
+# delete blue traffic
 $k delete svc svc-nodeport-blue
 
+# allow green traffic
 $k apply -f svc-nodeport-green.yml
 
+# now, close blue traffic
 $k delete deploy deploy-was-blue
 
+# display blue's status
 curl $(minikube ip):30000/actuator/health
 echo ""
 curl $(minikube ip):30000/version
