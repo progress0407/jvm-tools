312be301a4e869665d5106b5371df508d9ab44b6
progress0407 <progress0407@gmail.com>
2024-04-23 19:22:07 +0900

    JobConfiguration 수정 (Scope, JobParameter)

--- spring-batch/src/main/kotlin/philo/mjcoding/job/HelloJobCodingConfig.kt
+++ spring-batch/src/main/kotlin/philo/mjcoding/job/HelloJobCodingConfig.kt
@@ -3,6 +3,7 @@ package philo.mjcoding.job
 import mu.KotlinLogging
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.job.builder.JobBuilder
@@ -10,11 +11,13 @@ import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.support.AbstractPlatformTransactionManager
 
 @Configuration
+//@EnableBatchProcessing
 class HelloJobCodingConfig(
     private val jobRepository: JobRepository,
     private val transactionManager: AbstractPlatformTransactionManager
@@ -22,27 +25,42 @@ class HelloJobCodingConfig(
 
     private val log = KotlinLogging.logger { }
 
+    // --job.name=helloWorldJob requestDate=20240424
     @Bean
-    fun helloWorldJob(): Job {
-        log.info("hello world spring batch job !!")
+    fun helloWorldJob(helloWorldStep: Step): Job {
+
+        log.info(">>>> hello world spring batch job !!")
+
         return JobBuilder("helloWorldJob", jobRepository)
-            .start(helloWorldStep())
+            .start(helloWorldStep)
             .build()
     }
 
     @JobScope
     @Bean
-    fun helloWorldStep(): Step {
+    fun helloWorldStep(
+        helloWorldStepTasklet: Tasklet
+
+    ): Step {
+
+        log.info(">>>> this is helloWorldStep !!")
+
+
         return StepBuilder("helloWorldStep", jobRepository)
-            .tasklet(helloWorldStepTasklet(), transactionManager)
+            .tasklet(helloWorldStepTasklet, transactionManager)
             .build()
     }
 
     @StepScope
     @Bean
-    fun helloWorldStepTasklet(): Tasklet {
+    fun helloWorldStepTasklet(
+        @Value("#{jobParameters['requestDate']}") requestDate: String?
+    ): Tasklet {
+
+        log.info(">>>> hello world spring batch tasklet !!")
+        log.info { ">>>> input data, requestDate: $requestDate" }
+
         return Tasklet { _, _ ->
-            log.info("hello world spring batch tasklet !!")
             RepeatStatus.FINISHED
         }
     }
